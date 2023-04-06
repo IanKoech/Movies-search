21b8de5aab0de9dffdce785716843533ed8b1fad
IanKoech <iankoech9@gmail.com>
2023-04-07 01:16:27 +0300

    :construction: Fixed rendering error for movie cards

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { getAllMovies } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
-import { render } from 'node-sass';
+import '../MovieList/MovieList.scss';
 
 const MovieList = () => {
     const movies = useSelector(getAllMovies);
     let renderMovies = '';
+    
     // eslint-disable-next-line no-unused-vars
     renderMovies = movies.Response === 'True' ? (
-            movies.Search.map((movie, index) => {
-                <MovieCard key={index} data={movie} />
-            })
-        ) : (
-        <div className='movies-error'>
-            <h3>{movies.Error}</h3>
+        movies.Search.map((movie, index) => {
+            return <MovieCard key={index} data={movie} />
+        })
+    ) : (
+        <div className='movie-error'>
+            <h3>{movies.error}</h3>
         </div>
-    );
+    )
     console.log(movies);
     return (
-        <div className='movie-wrapper'> 
+        <div className='movie-wrapper'>
             <div className='movie-list'>
                 <h2>Movies</h2>
-                <div className='movie-container '>
-                    {renderMovies}
-                </div>
+                <div className='movie-container'>{renderMovies}</div>
+                
             </div>
         </div>
     );
