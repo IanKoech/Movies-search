f3f0e284e1394ce82970c8c45e1c501addeefa33
IanKoech <iankoech9@gmail.com>
2023-04-07 11:57:12 +0300

    :construction: Created async action for movue detail component

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice'; 
 
 const MovieDetail = () => {
-    return (
-        <div>
-            MovieDetail
+    const { imdbID } = useParams();
+    const dispatch  = useDispatch();
+    const data = useSelector(getSelectedMovieOrShow);
+
+    useEffect(() => {
+        dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+    }, [dispatch, imdbID])
+    return ( 
+        <div className='movie-section'>
+            <div className='section-left'>
+                <div className='movie-title'>
+                    {data.Title}
+                </div>
+                <div className='movie-rating'>
+                    <span>IMDB Rating <i className='fa fa-star'>{data.imdbRating}</i></span>
+                    <br></br>
+                    <span>IMDB Votes <i className='fa fa-thumbs-up'></i> : {data.imdbVotes}</span>
+                    <br></br>
+                    <span>Runtime <i className='fa fa-film'></i> : {data.Runtime}</span>
+                    <br></br>
+                    <span>Year <i className='fa fa-calendar'></i> : {data.Year}</span>
+                </div>
+                <div className='movie-plot'>
+                    {data.Plot}
+                </div>
+                <div className='movie-info'>
+                    <div>
+                        <span>Director</span>
+                        <span>{data.Director}</span>
+                    </div>
+                    <div>
+                        <span>Actors</span>
+                        <span>{data.Actors}</span>
+                    </div>
+                    <div>
+                        <span>Genre</span>
+                        <span>{data.Genre}</span>
+                    </div>
+                </div>
+            </div>
+            <div className='section-right'>
+                <img src={data.Poster} alt={data.Title} />
+            </div>
         </div>
     );
 };
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -7,18 +7,26 @@ export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', asyn
     const response = await  movieAPI.get(`?apiKey=${APIKey}&&s=${movieText}&&type=movie`
     );
     return response.data;
-})
+});
 
 export const fetchAsyncShows = createAsyncThunk('movies/fetchAsyncShows', async () => {
     const seriesText = "Friends";
     const response = await  movieAPI.get(`?apiKey=${APIKey}&&s=${seriesText}&&type=series`
     );
     return response.data;
+});
+
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk('movies/fetchAsyncMovieOrShowDetail',
+ async (id) => {
+    const response = await  movieAPI.get(`?apiKey=${APIKey}&&i=${id}&&Plot=full`
+    );
+    return response.data;
 })
 
 const initialState = {
     movies: {},
-    shows: {}
+    shows: {},
+    selectedMovieOrShow: {},
 }
 
 const movieSlice = createSlice({
@@ -44,10 +52,15 @@ const movieSlice = createSlice({
             console.log("Fetched successfully");
             return {...state, shows: payload}
         },
+        [fetchAsyncMovieOrShowDetail.fulfilled]: (state, {payload}) => {
+            console.log("Fetched successfully");
+            return {...state, selectedMovieOrShow: payload}
+        },
     },
 });
 
 export const {addMovies} = movieSlice.actions;
 export const getAllMovies = (state) =>state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) => state.movies.selectedMovieOrShow;
 export default movieSlice.reducer;
